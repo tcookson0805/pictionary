26a488dd839d218796a4e48a44942aa54b8fdbc0
Tyler Cookson <tcookson0805@gmail.com>
2016-08-22 10:44:58 -0700

    reconfigured to use event delegation for drawing on canvas, can now draw again

--- public/js/script.js
+++ public/js/script.js
@@ -3,39 +3,32 @@ var socket = io();
 
 // function where all the magic happens
 var pictionary = function() {
-    
     var canvas, context;
-    
     var drawing = false;
-
-    var draw = function(position) {
-        
+    
+    var draw = function(position) {    
         // this tells the context you are about to start drawing a new object
         context.beginPath();
-        
         // this is used to draw arcs, in this case we tell it to draw an entire circle
             //centered at position, with a radius of 6px
         context.arc(position.x, position.y,
                          6, 0, 2 * Math.PI);
-        
         // finally this fills the path in to create a solid black circle
         context.fill();
-        
         // EMITTER that lets server know 'draw' has occured and sends position  
+
         socket.emit('draw', position);       
     };
 
-    // using jQuery to select the <canvas> element
-    canvas = $('canvas');
-    
-    // using the .getContext('2d') function to create a drawing context for the canvas
-        // this context object allows you to draw simple graphis to the canvas
-    context = canvas[0].getContext('2d');
-    
-    // setting width and height of canvas object equal to its offsetWidth and offsetHeight
-        // this makes what is drawn to the context object display w/ correct resolutions
-    canvas[0].width = canvas[0].offsetWidth;
-    canvas[0].height = canvas[0].offsetHeight;
+    // // using jQuery to select the <canvas> element
+    // canvas = $('canvas');
+    // // using the .getContext('2d') function to create a drawing context for the canvas
+    //     // this context object allows you to draw simple graphis to the canvas
+    // context = canvas[0].getContext('2d');
+    // // setting width and height of canvas object equal to its offsetWidth and offsetHeight
+    //     // this makes what is drawn to the context object display w/ correct resolutions
+    // canvas[0].width = canvas[0].offsetWidth;
+    // canvas[0].height = canvas[0].offsetHeight;
     
    $('body').on('mousedown', canvas, function(){
         drawing = true;
@@ -45,35 +38,31 @@ var pictionary = function() {
         drawing = false;
    });
    
-   
     //  adding mousemove listener to the canvas
-    canvas.on('mousemove', function(event) {
-        
-        // first find the offset of the canvas on the page
-        var offset = canvas.offset();
-        
-        // then subtract this from the event pageX and pageY attributes
-            // note that the page attributes give position of the mouse relative to the whole
-            //  page so by subtracting the offset we obtain the position of the mouse relative
-            //  to the top-left of the canvas
-        var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-        
+    $('body').on('mousemove', canvas, function(event) {
+
         if(drawing){
+            
+            // first find the offset of the canvas on the page
+            var offset = canvas.offset();
+            // then subtract this from the event pageX and pageY attributes
+                // note that the page attributes give position of the mouse relative to the whole
+                //  page so by subtracting the offset we obtain the position of the mouse relative
+                //  to the top-left of the canvas
+            var position = {x: event.pageX - offset.left,
+                            y: event.pageY - offset.top};
+            
             draw(position);            
         }
-  
     });
     
     // hears the 'draw' from the server
     socket.on('draw', function(position){
+
        draw(position);
     });
-    
-    
-    
+
     //////////////////
-    
     
     var guessBox;
     var guessDiv = $('.guess').clone();
@@ -91,18 +80,57 @@ var pictionary = function() {
     guessBox = $('#guess input');
     guessBox.on('keydown', onKeyDown);
     
-    
     var guessDiv = '<div class="guess"><span class="guess_text"> {{guess}}</span></div>'  
 
     var guessTemplate = Handlebars.compile(guessDiv)
     
-    
     socket.on('guess', function(value){
         var userguess = guessTemplate({guess : value});
         $('.guessboard').append(userguess)
-    })
+    });
+  
     
+    // defer.promise(closeSignin);
+    $('body').on('keydown', '.hero', function(event){
+        if(event.keyCode != 13){
+            return;
+        }
+        var val = $('#screen_name').val();
+        socket.emit('screenName', val);
+    });
+    
+    socket.on('screenName', function(val){
+
+        if(val === 'already exists'){
+            $('.alreadyTaken').show();
+        }else{
+            $('.signIn').hide();
+            $('.game').show();
+            
+            // $('.user_info_name').html
+            // using jQuery to select the <canvas> element
+            canvas = $('canvas');
+            // using the .getContext('2d') function to create a drawing context for the canvas
+                // this context object allows you to draw simple graphis to the canvas
+            context = canvas[0].getContext('2d');
+            // setting width and height of canvas object equal to its offsetWidth and offsetHeight
+                // this makes what is drawn to the context object display w/ correct resolutions
+            canvas[0].width = canvas[0].offsetWidth;
+            canvas[0].height = canvas[0].offsetHeight;
+        }
+    });
+    
+    
+      
 };
+
+
+
+
+
+
+
+
 
 $(document).ready(function() {
     pictionary();
--- server.js
+++ server.js
@@ -4,12 +4,40 @@ var socket_io = require('socket.io')
 
 var app = express();
 app.use(express.static('public'));
-app.use('/css', express.static(__dirname + '/node_modules/materialize-css/dist/'))
+app.use('/css', express.static(__dirname + '/node_modules/materialize-css/dist'))
+app.use('/js', express.static(__dirname + '/node_modules/materialize-css/dist'))
+
 var server = http.Server(app);
 var io = socket_io(server);
 
+var usersOnline = {}
+
+var User = function(name){
+  this.screenName = name;
+  this.wins = 0;
+  this.drawer = false;
+};
+
+
 // establishing listener for connection
 io.on('connection', function(socket){
+  
+  socket.on('screenName', function(val){
+    
+    if(usersOnline[val]){
+      io.emit('screenName', 'already exists');
+    }else{
+      usersOnline[val] = new User(val);
+      
+      socket.user = usersOnline[val];
+      
+      if(!Object.keys(usersOnline).length){
+        usersOnline.val.drawer = true;
+      }
+       
+      io.emit('screenName', usersOnline[val].name);
+    }
+  });
   
   // socket listens for 'draw' event to occur
   socket.on('draw', function(position){
@@ -20,6 +48,37 @@ io.on('connection', function(socket){
   socket.on('guess', function(value){
     io.emit('guess', value);
   })
+  
+  socket.on('disconnect', function(){
+    // check to see if disconnecting user is the drawer
+    
+    console.log(usersOnline);
+    
+    if(Object.keys(usersOnline).length){
+      if(socket.user.drawer && Object.keys(usersOnline).length > 1){
+        
+        // removing disconnecting user from userOnline obj
+        delete usersOnline[socket.user.name];
+        
+        var randomNum = Math.floor(Math.random() * Object.keys(usersOnline).length);
+        var count = 0;
+        
+        for(var key in usersOnline){
+          if(count === randomNumber){
+            usersOnline[key].drawer = true;
+          }else{
+            count++;
+          }
+        }
+          
+      }else{
+        delete usersOnline[socket.user.name];  
+      } 
+    }
+  });
+  
+  
+  
 
 });
 
